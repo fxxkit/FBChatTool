806586b8437ee456ac9fe34af0a495003a6408f3
Jordan Hsu <jordan00830@gmail.com>
2014-03-07 15:08:38 +0800

    add features => dashed border while dragging

--- contentScript.js
+++ contentScript.js
@@ -2,45 +2,54 @@ $(function(){
 	console.log('This is FBDragger!!');
 
 	// Initialize the opened chat room
-	FBDragger.appendBtn();
-	FBDragger.bindDragEvent();
+	FB_ChatRoomDragger.appendBtn();
+	FB_ChatRoomDragger.bindDragEvent();
 
 
 	// Append btn & Bind Event when click side friend
 	$('._42fz').on('click',function(){
 		console.log('click side friends for chat room');
-		var delayAppendBtn = _.bind(FBDragger.appendBtn, FBDragger);
+		var delayAppendBtn = _.bind(FB_ChatRoomDragger.appendBtn, FB_ChatRoomDragger);
         _.delay(delayAppendBtn,1500);
 		
-		var delayDragEvent = _.bind(FBDragger.bindDragEvent,FBDragger);
+		var delayDragEvent = _.bind(FB_ChatRoomDragger.bindDragEvent,FB_ChatRoomDragger);
 		_.delay(delayDragEvent,2000);			
 	});
 
 	// Append btn & Bind Event when click top friend
 	$('.jewelContent').on('click',function(){
 		console.log('click top friends for chat room');
-		var delayAppendBtn = _.bind(FBDragger.appendBtn, FBDragger);
+		var delayAppendBtn = _.bind(FB_ChatRoomDragger.appendBtn, FB_ChatRoomDragger);
         _.delay(delayAppendBtn,1500);
 		
-		var delayDragEvent = _.bind(FBDragger.bindDragEvent,FBDragger);
+		var delayDragEvent = _.bind(FB_ChatRoomDragger.bindDragEvent,FB_ChatRoomDragger);
 		_.delay(delayDragEvent,2000);			
 	});
 
+
+	// Add sidebar control btn
+	//FB_ChatSidebarBtn.appendBtn();
+
 });
 
-var FBDragger = {
+var FB_ChatRoomDragger = {
 	preX_offset_: 0,
-
 	appendBtn: function(){
+		var oFBCRD = this;
 		$('.fbNubFlyoutTitlebar').each(function(idx,el){
 			var currentChatDOM = this;
-			if(!$(currentChatDOM).prev().hasClass('FB-chat-drag-btn')){
+			// Prevent duplicated button append
+			if(!$(currentChatDOM).prev().prev().hasClass('FB-chat-drag-btn')){
 				$(currentChatDOM).before("<span class='FB-chat-drag-btn glyphicon glyphicon-chevron-up btn btn-default btn-xs'></span>");
 				
-				// Reset the wrapper height to prevent push element
+				// Reset the wrapper height to prevent push element down
 				var wrapperEl = $(currentChatDOM).parent().parent().parent();
 				var currentChatDOM_height = $(wrapperEl).height();
 				$(wrapperEl).css({'height': currentChatDOM_height + 16});
+
+				// Add fake border
+				var fakeBorder = $('<div/>').addClass('fb-chat-fake-border');
+				$(currentChatDOM).before(fakeBorder);
 			}
 		});
 	},
@@ -55,6 +64,8 @@ var FBDragger = {
 				console.log('start dragging the btn!!!!');
 				//console.log(ui.offset);
 				preX_offset = ui.offset.top;
+				// Show fake border
+				$(this).next().css({'display':'block'});
 			},
 			stop: function(event, ui ){
 				console.log('stop dragging the btn!!!!');
@@ -64,13 +75,13 @@ var FBDragger = {
 
 				var newHeight = 0;
 
-				// Conversation div
-				var resizeTarget = $(this).next().next().next();
+				// Conversation div (.fbNubFlyoutBody)
+				var resizeTarget = $(this).next().next().next().next();
 				var resizeTarget_height = $(resizeTarget).height();
 				var newHeight = resizeTarget_height - xDistance;
 				$(resizeTarget).height(newHeight);
 				
-				// Outest wrapper
+				// Outest wrapper (.fbNubFlyout .fbDockChatTabFlyout)
 				var resizeTarget2 = $(this).parent().parent().parent();
 				var resizeTarget_height2 = $(resizeTarget2).height();
 				var newHeight = resizeTarget_height2 - xDistance;
@@ -78,10 +89,22 @@ var FBDragger = {
 
 				//resetting the btn position
 				$(this).css({'top': 0});
+				//resetting fake border position
+				$(this).next().css({'top': 16});
+				// Hide fake border
+				$(this).next().css({'display':'none'});				
 			},
 			drag: function(event, ui ){
 				console.log('dragging event triggered');
+				var fakeBorder = $(this).next();
+				$(fakeBorder).css({'top': ui.position.top + 16});
 			},
 		});
+	}
+};
+
+var FB_ChatSidebarBtn = {
+	appendBtn: function(){
+		$('.fbChatSidebar').append("<span class='FB-chat-sidebar-btn glyphicon glyphicon-chevron-up btn btn-default btn-xs'></span>");
 	}
 }
